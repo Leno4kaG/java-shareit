799bfb809fbe28f437ff6f6e72c02e6011f163c4
elena <leno4kagaripova@yandex.ru>
2023-06-17 00:03:00 +0800

    ТЗ-16. Добавлен докер и разделено на 2 модуля, gateway и server.

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingClientController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingClientController.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.booking;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -27,38 +26,38 @@ public class BookingClientController {
 
     @PostMapping
     public ResponseEntity<Object> createBooking(@RequestHeader("X-Sharer-User-Id") long userId,
-                                                    @Valid @RequestBody BookingClientRequestDto bookingClientRequestDto) {
+                                                @Valid @RequestBody BookingClientRequestDto bookingClientRequestDto) {
         log.info("Create booking with user id {},booking client={}", userId, bookingClientRequestDto);
         return bookingService.createBooking(userId, bookingClientRequestDto);
     }
 
     @PatchMapping("/{bookingId}")
     public ResponseEntity<Object> updateBooking(@RequestHeader("X-Sharer-User-Id") long userId,
-                                                    @PathVariable long bookingId,
-                                                    @RequestParam(name = "approved") Boolean approved) {
+                                                @PathVariable long bookingId,
+                                                @RequestParam(name = "approved") Boolean approved) {
         log.info("update booking booking id {}", bookingId);
         return bookingService.updateBooking(userId, bookingId, approved);
     }
 
     @GetMapping("/{bookingId}")
     public ResponseEntity<Object> getBooking(@RequestHeader("X-Sharer-User-Id") long userId,
-                                                 @PathVariable long bookingId) {
+                                             @PathVariable long bookingId) {
         return bookingService.getBooking(userId, bookingId);
     }
 
     @GetMapping
     public ResponseEntity<Object> getBookingsForCurrentUser(@PositiveOrZero @RequestParam(name = "from", defaultValue = "0") Integer from,
-                                                      @Positive @RequestParam(name = "size", defaultValue = "10") Integer size,
-                                                      @RequestHeader("X-Sharer-User-Id") long userId,
-                                                      @RequestParam(name = "state", defaultValue = "ALL") BookingClientState state) {
+                                                            @Positive @RequestParam(name = "size", defaultValue = "10") Integer size,
+                                                            @RequestHeader("X-Sharer-User-Id") long userId,
+                                                            @RequestParam(name = "state", defaultValue = "ALL") BookingClientState state) {
         return bookingService.getBookingsForCurrentUser(userId, state, from, size);
     }
 
     @GetMapping("/owner")
     public ResponseEntity<Object> getBookingsForAllItems(@PositiveOrZero @RequestParam(name = "from", defaultValue = "0") Integer from,
-                                                   @Positive @RequestParam(name = "size", defaultValue = "10") Integer size,
-                                                   @RequestHeader("X-Sharer-User-Id") long userId,
-                                                   @RequestParam(name = "state", defaultValue = "ALL") BookingClientState state) {
+                                                         @Positive @RequestParam(name = "size", defaultValue = "10") Integer size,
+                                                         @RequestHeader("X-Sharer-User-Id") long userId,
+                                                         @RequestParam(name = "state", defaultValue = "ALL") BookingClientState state) {
 
         return bookingService.getBookingsForAllItems(userId, state, from, size);
     }
